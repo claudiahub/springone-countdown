69493682975a62540f39e31e8513bfcb8e8f7b51
Claudia Bressi <cbressi@vmware.com>
2023-01-18 23:45:59 +0100

    Polish controller

--- src/main/java/com/demo/springonecountdown/SpringoneController.java
+++ src/main/java/com/demo/springonecountdown/SpringoneController.java
@@ -3,6 +3,7 @@ package com.demo.springonecountdown;
 import java.time.Duration;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,16 +12,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 public class SpringoneController {
 
-  ZoneId zoneId = ZoneId.of ( "America/NewYork" );
-  ZonedDateTime now = ZonedDateTime.now ( zoneId );
-  ZonedDateTime springOneStartDate = now.plusDays(10);
-  Duration duration = Duration.between ( now , springOneStartDate );
+  ZoneId zoneId = ZoneId.of( "America/Chicago" );
+  ZonedDateTime now = ZonedDateTime.now( zoneId );
+  final static DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a");
+  ZonedDateTime springOneStartDate = ZonedDateTime.parse("2023-01-24 17:00:00 pm", formatter);
+  Duration duration = Duration.between( now , springOneStartDate );
   long countdown = duration.toHours();
 
-@RequestMapping("/")
+  @RequestMapping("/")
 
   public String index() {
-    return "SpringOne happens in "+countdown+"!";
+    return "SpringOne happens in " + Math.floor(countdown) + " hours !";
   }
 
 }
