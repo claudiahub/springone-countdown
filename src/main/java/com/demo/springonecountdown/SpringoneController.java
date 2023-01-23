8a24835d8bfd63a7d94737e73a92ffd594226ce2
Claudia Bressi <cbressi@vmware.com>
2023-01-23 12:56:34 +0100

    Set zonedate formatter

--- src/main/java/com/demo/springonecountdown/SpringoneController.java
+++ src/main/java/com/demo/springonecountdown/SpringoneController.java
@@ -14,15 +14,15 @@ public class SpringoneController {
 
   ZoneId zoneId = ZoneId.of( "America/Chicago" );
   ZonedDateTime now = ZonedDateTime.now( zoneId );
-  DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss a z");
-  ZonedDateTime springOneStartDate = ZonedDateTime.parse("2023-01-24 17:00:00 pm +01:00", formatter);
+  DateTimeFormatter formatter  = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss z");
+  ZonedDateTime springOneStartDate = ZonedDateTime.parse("2023-01-24 17:00:00 +01:00", formatter);
   Duration duration = Duration.between( now , springOneStartDate );
   long countdown = duration.toHours();
 
   @RequestMapping("/")
 
   public String index() {
-    return "SpringOne happens in hours";
+    return "SpringOne happens in+"+Math.floor(countdown)+" hours";
   }
 
 }
